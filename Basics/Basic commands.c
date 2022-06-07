d5a2fb4e0cacb300f93f3be9bce3827b5087010f
HarmanSMann <76751076+HarmanSMann@users.noreply.github.com>
2022-06-07 16:41:03 -0400

    Update Basic commands.c

--- Basics/Basic commands.c
+++ Basics/Basic commands.c
@@ -7,12 +7,18 @@ Trying to understand syntac and stuff
 
 int main(){
   printf("hello world");
+
+
+  // Using addresses --------------------------------------------------------------------------
+  int age; // create a variable, it will contain an address location
+  scanf("%d", &age); //Use & to access the address location
+  printf('%d', age);
+  
+  // Variables --------------------------------------------------------------------------------
+  int num1 = 1;
+  float num2 = 1.0;
+  char = 's';
+  char [] = 'string';
+  printf("%f", num2)
+
 }
-
-// Using addresses
-int age; // create a variable, it will contain an address location
-scanf("%d", &age); //Use & to access the address location
-
-printf(age);
-printf(%age);
-
