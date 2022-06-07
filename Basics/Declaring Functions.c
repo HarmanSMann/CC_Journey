c567fa344be458f757b5721f3ceada921c2b5cf9
HarmanSMann <76751076+HarmanSMann@users.noreply.github.com>
2022-06-07 16:48:05 -0400

    Create Declaring Functions.c

---
+++ Basics/Declaring Functions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+float sum(float, float); // declare a function before hand
+
+int main()
+{
+  float x, y;
+  x = 10.0;
+  y = 11.1;
+  
+  result = sum(x, y);
+  printf("%f", result);
+}
+
+float sum(float i, float j) // write it out after main
+{
+ return i + j; 
+}
